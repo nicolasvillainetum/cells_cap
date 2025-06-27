3b33d0b2c5b05ee8390a979355f86540af20d4d9
nicolas.villa.contractor <nicolas.villa.contractor@bbva.com>
2025-06-27 15:32:58 -0500

    add: introduccion js

---
+++ introduccion_js/app.js
@@ -0,0 +1,37 @@
+
+let texto = "Hola mundo";
+let numero = 42;
+let boleanito = true;
+let nulo = null;
+let indefinido;
+let simbolo = Symbol();
+let numeroGrande = 1234567890123456789012345678901234567890n;
+
+console.groupCollapsed("grupo");
+console.log("String:", texto);
+console.error("Number:", numero);
+console.info("Boolean:", boleanito);
+console.log("Null:", nulo);
+console.warn("Undefined:", indefinido);
+console.debug("Symbol:", simbolo);
+console.info("BigInt:", numeroGrande);
+console.groupEnd();
+
+
+texto = "adios mundo";
+numero = 1;
+boleanito = false;
+nulo = 2;
+indefinido = 1;
+simbolo = Symbol("hola");
+numeroGrande = 123456778901234567890n;
+
+console.groupCollapsed("nuevo grupo");
+console.debug("String:", texto);
+console.debug("Number:", numero);
+console.debug("Boolean:", boleanito);
+console.debug("Null:", nulo);
+console.debug("Undefined:", indefinido);
+console.debug("Symbol:", simbolo);
+console.debug("BigInt:", numeroGrande);
+console.groupEnd();
