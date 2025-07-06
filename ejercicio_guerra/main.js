95533cb09dc7d3f5cefac54b423d407a5af228c1
nicolas.villa.contractor <nicolas.villa.contractor@bbva.com>
2025-07-05 22:28:36 -0500

    ejercicio guerra

--- ejercicio_guerra/main.js
+++ ejercicio_guerra/main.js
@@ -23,75 +23,206 @@ let registro_rojo= [];
 
 
 equipo1= generadorEquipo();
-console.log("Equipo Rojo: ", equipo1);
+//console.log("Equipo Rojo: ", JSON.stringify(equipo1));
 equipo2= generadorEquipo();
-console.log("Equipo Azul: ", equipo2);
+//console.log("Equipo 2: ", equipo2);
 
 
 //let ganador = randomGenerator(0,1);
-//console.log("Equipo Rojo: ", equipo1.tropas[1]);
+////console.log("Equipo Rojo: ", equipo1.tropas[1]);
 
 
 iniciarGuerra(equipo1, equipo2);
+//prueba();
+
+
+function prueba(){
+
+    //console.log("iniciando prueba");
+
+
+let equipo_prueba = {
+  "tropas": [
+    [ // Escuadrón 1
+      {"nombre":"Arquero","activo":false,"vida":0,"ataque":0}, // Inactivo
+      {"nombre":"Arquero","activo":true,"vida":50,"ataque":15},
+      {"nombre":"Arquero","activo":true,"vida":50,"ataque":15},
+      {"nombre":"Arquero","activo":true,"vida":50,"ataque":15},
+      {"nombre":"Arquero","activo":true,"vida":50,"ataque":15},
+      {"nombre":"Arquero","activo":true,"vida":50,"ataque":15},
+      {"nombre":"Arquero","activo":true,"vida":50,"ataque":15},
+      {"nombre":"Arquero","activo":true,"vida":50,"ataque":15},
+
+    ],
+    [ // Escuadrón 1
+      {"nombre":"Arquero","activo":false,"vida":0,"ataque":0}, // Inactivo
+      {"nombre":"Arquero","activo":true,"vida":50,"ataque":15},
+      {"nombre":"Arquero","activo":true,"vida":50,"ataque":15},
+      {"nombre":"Arquero","activo":true,"vida":50,"ataque":15},
+      {"nombre":"Arquero","activo":true,"vida":50,"ataque":15},
+      {"nombre":"Arquero","activo":true,"vida":50,"ataque":15},
+      {"nombre":"Arquero","activo":true,"vida":50,"ataque":15},
+
+    ],
+    [ // Escuadrón 3
+      {"nombre":"Caballero","activo":false,"vida":0,"ataque":0}, // Inactivo
+      {"nombre":"Caballero","activo":false,"vida":0,"ataque":0},
+      {"nombre":"Caballero","activo":false,"vida":0,"ataque":0},
+      {"nombre":"Caballero","activo":false,"vida":0,"ataque":0},
+      {"nombre":"Caballero","activo":false,"vida":0,"ataque":0},
+      {"nombre":"Caballero","activo":false,"vida":0,"ataque":0},
+
+    ]
+  ]
+};
+
+  //console.log("Equipo 1: ", equipo_prueba);
+  let equipo_prueba_aux;
+
+  for (let i = 0; i < equipo_prueba.tropas.length; i++) {
+              for (let j =equipo_prueba.tropas[i].length; j > 0; j--) {
+
+
+             equipo_prueba_aux= equipo_prueba.tropas[0].splice(j, 1);
+      }
+  }
+
+    
+  equipo_prueba= equipo_prueba_aux;
+
+  //console.log("Equipo aux: despues del splice ", equipo_prueba_aux);
+  //console.log("Equipo 1: despues del splice ", equipo_prueba);
+}
 
 function iniciarGuerra(equipo1, equipo2) {
 
-  console.log("guerras iniciada");
-  console.log("longitud equipo rojo: ", equipo1.tropas[1].length);
+  //console.log("guerras iniciada");
+  let equipo1_aux= equipo1;
+  let equipo2_aux = equipo2;
 
+  let ganador= false;
+
+while(!ganador){
+  console.log("ATAQUE EQUIPO 1");
   for (let i = 0; i < equipo1.tropas.length; i++) {
     let escuadron_act = equipo1.tropas[i];
-    console.log("Tropa " + (i + 1) + ":");
     for (let j = 0; j < escuadron_act.length; j++) {
 
-      console.log("Nombre: " + escuadron_act[j].nombre);
-      console.log("Vida: " + escuadron_act[j].vida);
-      let ataque= randomGenerator(1,escuadron_act[j].ataque ); // Simula un ataque aleatorio
-      ataque = ataque * (1 - (randomGenerator(0, 30)/100)) // Simula un ataque aleatorio 
-      console.log("Ataque: " + ataque);
 
-      let tropa_atacada= randomGenerator(0,equipo2.tropas[i].length-1);
-      console.log("Vida tropa equipo 2: " +  equipo2.tropas[i][tropa_atacada].vida);
+      // //console.log("Nombre: " + escuadron_act[j].nombre);
+      // //console.log("Vida: " + escuadron_act[j].vida);
+      let ataque= randomGenerator(1,escuadron_act[j].ataque ); // ataque aleatorio
+      ataque = ataque * (1 - (randomGenerator(0, 30)/100)) // inclemencias del clima
 
-      equipo2.tropas[i][tropa_atacada].vida -= ataque;
+      let escuadron_atacado=0;
+      let tropa_atacada=0;
+      
+      do{
+                if ((equipo2_aux.tropas.length == 0)){break;}
 
-      if( equipo2.tropas[i][tropa_atacada].vida <= 0){ 
-        console.log("Tropa eliminada: " + equipo2.tropas[i][tropa_atacada].nombre);
-        equipo2.tropas[i].splice(tropa_atacada, 1);
+        escuadron_atacado= randomGenerator(0, (equipo2_aux.tropas.length)-1);
+        tropa_atacada= randomGenerator(0, (equipo2_aux.tropas[escuadron_atacado].length)-1);
+      }
+      while( (typeof (equipo2_aux.tropas[escuadron_atacado][tropa_atacada]) =='undefined') && (equipo2_aux.tropas.length > 0) );      
+      
+
+      //console.log("Vida equipo 2 antes del ataque: " +  equipo2_aux.tropas[escuadron_atacado][tropa_atacada].vida);
+      //console.log("Tropa atacada: " , equipo2_aux.tropas[escuadron_atacado][tropa_atacada]);
+
+      
+      equipo2_aux.tropas[escuadron_atacado][tropa_atacada].vida -= ataque;
+      //console.log("Ataque: " + ataque);
+      //console.log("Tropa atacada: " , equipo2_aux.tropas[escuadron_atacado][tropa_atacada]);
+      //console.log("Vida tropa equipo 2 despues del ataque: " +  equipo2_aux.tropas[escuadron_atacado][tropa_atacada].vida);
+
+      if( equipo2_aux.tropas[escuadron_atacado][tropa_atacada].vida <= 0){ 
+        //console.log("Tropa eliminada: " + equipo2_aux.tropas[escuadron_atacado][tropa_atacada].nombre);
+        equipo2_aux.tropas[escuadron_atacado].splice(tropa_atacada, 1);
+                if ((equipo2_aux.tropas.length == 0)){break;}
+
 
       }
+
+                    if ((equipo2_aux.tropas.length == 0)){break;}
+
+
+
     }
 
   }
 
+  //equipo2= equipo2_aux;
+
+    console.log("equipo2 despues del ataque: ", equipo2_aux);
 
 
-  for (let i = 0; i < equipo2.tropas.length; i++) {
-    let escuadron_act = equipo2.tropas[i];
-    console.log("Tropa " + (i + 1) + ":");
+  if((equipo2_aux.tropas.length == 0)){
+    console.log("Equipo 1 ha ganado la guerra");
+    ganador = true;
+    break;
+  }
+
+    console.log("ATAQUE EQUIPO 2");
+
+  for (let i = 0; i < equipo2_aux.tropas.length; i++) {
+    let escuadron_act = equipo2_aux.tropas[i];
     for (let j = 0; j < escuadron_act.length; j++) {
 
-      console.log("Nombre: " + escuadron_act[j].nombre);
-      console.log("Vida: " + escuadron_act[j].vida);
-      let ataque= randomGenerator(1,escuadron_act[j].ataque ); // Simula un ataque aleatorio
-      ataque = ataque * (1 - (randomGenerator(0, 30)/100)) // Simula un ataque aleatorio 
-      console.log("Ataque: " + ataque);
 
-      let tropa_atacada= randomGenerator(0,equipo1.tropas[i].length-1);
-      console.log("Vida tropa equipo 2: " +  equipo1.tropas[i][tropa_atacada].vida);
+      // //console.log("Nombre: " + escuadron_act[j].nombre);
+      // //console.log("Vida: " + escuadron_act[j].vida);
+      let ataque= randomGenerator(1,escuadron_act[j].ataque ); // ataque aleatorio
+      ataque = ataque * (1 - (randomGenerator(0, 30)/100)) // inclemencias del clima
 
-      equipo1.tropas[i][tropa_atacada].vida -= ataque;
+      let escuadron_atacado=0;
+      let tropa_atacada=0;
+      
+      do{
 
-      if( equipo1.tropas[i][tropa_atacada].vida <= 0){ 
-        console.log("Tropa eliminada: " + equipo1.tropas[i][jtropa_atacada].nombre);
-        equipo1.tropas[i].splice(tropa_atacada, 1);
+        if ((equipo1_aux.tropas.length == 0)){break;}
+
+        escuadron_atacado= randomGenerator(0, (equipo1_aux.tropas.length)-1);
+        tropa_atacada= randomGenerator(0, (equipo1_aux.tropas[escuadron_atacado].length)-1);
+      }
+      while( (typeof (equipo1_aux.tropas[escuadron_atacado][tropa_atacada]) =='undefined')  );      
+      
+
+      //console.log("Vida equipo 2 antes del ataque: " +  equipo1_aux.tropas[escuadron_atacado][tropa_atacada].vida);
+      //console.log("Tropa atacada: " , equipo1_aux.tropas[escuadron_atacado][tropa_atacada]);
+
+      
+      equipo1_aux.tropas[escuadron_atacado][tropa_atacada].vida -= ataque;
+      //console.log("Ataque: " + ataque);
+      //console.log("Tropa atacada: " , equipo1_aux.tropas[escuadron_atacado][tropa_atacada]);
+      //console.log("Vida tropa equipo 2 despues del ataque: " +  equipo1_aux.tropas[escuadron_atacado][tropa_atacada].vida);
+
+      if( equipo1_aux.tropas[escuadron_atacado][tropa_atacada].vida <= 0){ 
+        //console.log("Tropa eliminada: " + equipo1_aux.tropas[escuadron_atacado][tropa_atacada].nombre);
+        equipo1_aux.tropas[escuadron_atacado].splice(tropa_atacada, 1);
+
+                if ((equipo1_aux.tropas.length == 0)){break;}
+
 
       }
+
+            if ((equipo1_aux.tropas.length == 0)){break;}
+
+
+
     }
 
   }
 
+    if((equipo1_aux.tropas.length == 0)){
+    console.log("Equipo 2 ha ganado la guerra");
+    ganador = true;
+    break;
+  }
 
+      console.log("equipo1 despues del ataque: ", equipo1_aux);
+
+
+}
 
 }
 
