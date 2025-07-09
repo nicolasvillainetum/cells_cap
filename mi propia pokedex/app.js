e9631d38792fbcc30f255e924d46b98f4d4f77e8
nicolas.villa.contractor <nicolas.villa.contractor@bbva.com>
2025-07-09 13:10:41 -0500

    mi propia pokedex

--- mi propia pokedex/app.js
+++ mi propia pokedex/app.js
@@ -1,14 +1,11 @@
+let tpl = document.getElementById('tarjeta');
+let clon = tpl.content.cloneNode(true);
+document.body.appendChild(clon);
 
-  let tpl = document.getElementById('tarjeta');
-  let nuevoClon = tpl.content.cloneNode(true);
-  let clon = tpl.content.cloneNode(true);
+let lista_pokemons = document.getElementById('poke_listado');
+let POKE_URL='https://pokeapi.co/api/v2/pokemon/';
 
-  document.body.appendChild(clon);
-
-let POKE_URL='https://pokeapi.co/api/v2/pokemon/1';
-
-
-async function nombre() { 
+async function infoTarjeta(poke_id) { 
     const elemento_nombre = document.getElementById('nombre_poke');
 
     const imagen_frente = document.getElementById('imagen_frente');
@@ -17,9 +14,18 @@ async function nombre() {
 
     const info_poke = document.getElementById('info_poke');
 
+    if(poke_id==null){
+    imagen_frente.alt = "...";
+    imagen_lateral.alt = "...";
+    imagen_shiny.alt = "...";
+
+    info_poke.textContent = "...";
+    return;
+    }
+
   try{  
 
-    const { name, sprites, abilities } = await (await fetch(POKE_URL)).json();
+    const { name, sprites, abilities } = await (await fetch(POKE_URL+poke_id)).json();
     elemento_nombre.textContent = name;
 
     imagen_frente.src = sprites.front_default;
@@ -42,5 +48,65 @@ async function nombre() {
 }
 
 
-// document.addEventListener('DOMContentLoaded', habilidades);
-document.addEventListener('DOMContentLoaded', nombre);
+async function listado() { 
+    
+    const POKE_URL_LIST='https://pokeapi.co/api/v2/pokemon/';
+    const LIMIT = 10;
+
+    let pokemones_completos = [];
+
+  try{  
+
+    const response = await fetch(`${POKE_URL_LIST}?limit=${LIMIT}`);
+    const data = await response.json();
+    const pokemons_data = data.results;
+
+
+    const pokemones_url = pokemons_data.map(async (p) => {
+        const res = await fetch(p.url);
+        return res.json();
+    });
+
+    pokemones_completos = await Promise.all(pokemones_url);
+    listarPokemones(pokemones_completos);
+
+} catch (error) {
+  console.error('Error al obtener el nombre:', error);
+  return;}
+  
+}
+
+function listarPokemones(pokemones_completos){
+    pokemones_completos.forEach(pokemon => vistaPreviaPokemon(pokemon));
+}
+
+function vistaPreviaPokemon(pokemon) {
+    let clon_lista = lista_pokemons.content.cloneNode(true);
+    const info_poke_listado = clon_lista.querySelector(".info_poke_listado");
+
+
+
+    const nombre = clon_lista.getElementById('nombre_poke_lst');
+    const imagen_frente = clon_lista.getElementById('imagen_frente_lst');
+    const tipo_poke = clon_lista.getElementById('tipo_poke_list');
+
+    nombre.textContent = pokemon.name;
+    imagen_frente.src = pokemon.sprites.front_default;
+
+    const tipos = pokemon.types.map(t => t.type.name).join(', ');
+    tipo_poke.textContent = tipos;
+
+
+    info_poke_listado.addEventListener("click", ()=>{
+
+      infoTarjeta(pokemon.id)
+    });
+
+
+    document.body.appendChild(clon_lista);
+}
+document.addEventListener('DOMContentLoaded',()=>{
+  listado();
+  infoTarjeta()
+});
+//document.addEventListener('DOMContentLoaded', listado);
